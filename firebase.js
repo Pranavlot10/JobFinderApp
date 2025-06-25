7b21cd4f700435c1d956fe9ceef7c589e63386f0
Pranav <pranavlot10@gmail.com>
2025-06-25 16:35:00 +0530

    feat: Configure Firebase with environment variables and add dotenv support

--- firebase.js
+++ firebase.js
@@ -3,17 +3,25 @@ import { initializeApp, getApps } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZuQbEVNYbEwxeSSIeR-sPPoQq7x6cYEo",
-  authDomain: "jobfinderapp-f07de.firebaseapp.com",
-  projectId: "jobfinderapp-f07de",
-  storageBucket: "jobfinderapp-f07de.appspot.com",
-  messagingSenderId: "107435304602",
-  appId: "1:107435304602:web:9592d5a83499b1e9876123",
-  measurementId: "G-TS1BFJ2MQ5",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
-
 let app;
 if (getApps().length === 0) {
   app = initializeApp(firebaseConfig);
